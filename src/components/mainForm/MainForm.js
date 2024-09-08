74704071d8a4a6681c98a823a6732863b9f7dfdb
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-09-08 16:12:44 +0700

    created more loaded function

--- src/components/mainForm/MainForm.js
+++ src/components/mainForm/MainForm.js
@@ -8,10 +8,10 @@ const MainForm = () => {
 
     const [inputValue, setInputValue] = useState('');
     const [gifList, setGifList] = useState([]);
+    const [_baseLimit, _setBaseLimit] = useState(8)
 
     const   _apiBase = 'https://api.giphy.com/v1/gifs/',
-            _apiKey = 'api_key=gG7GN2QoqDL8uN2HNZivgL42pB68Tq1j',
-            _baseLimit = 24;
+            _apiKey = 'api_key=gG7GN2QoqDL8uN2HNZivgL42pB68Tq1j';
 
     const getGif = (question = 'anime') => {
         fetch(`${_apiBase}search?${_apiKey}&q=${question}&limit=${_baseLimit}`)
@@ -23,6 +23,8 @@ const MainForm = () => {
             return response.json();
         })
         .then((data) => setGifList(data))
+
+        console.log('zapros')
     }
 
     const searchGiphy = () => {
@@ -32,9 +34,9 @@ const MainForm = () => {
     const renderItems = (arr) => {
         const items = arr.data.map((item) => {
             return (
-                <a href={item.url} className="form__wrapper-link"><img key={item.id} 
+                <a href={item.url} className="form__wrapper-link" key={item.id}><img
                 src={item.images.original.url} 
-                alt={item.username} 
+                alt={item.username}
                 className="form__wrapper-item"/></a>
             )
         })
@@ -51,12 +53,17 @@ const MainForm = () => {
         }
     }
 
+    const loadMore = () => {
+        _setBaseLimit(_baseLimit => _baseLimit + 8)
+        getGif(inputValue)
+    }
+
     return (
         <div className="container">
             <div className="form">
                 <div className="form__request">
-                    <input type='text' placeholder='The name of the gif animation' required className="form__request-input" value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyUp={onKeyUp}>
-
+                    <input type='text' placeholder='The name of the gif animation' className="form__request-input"
+                           value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyUp={onKeyUp}>
                     </input>
                     <button type="button" onClick={searchGiphy} className="form__request-button">
                         <img src={searchImg} alt="form__request-button-img" />
@@ -65,13 +72,25 @@ const MainForm = () => {
                 <div className="form__popular">
                     <div className="form__popular-title form__title">
                         <img src={popularImg} alt="popular smile" />
-                        {gifList.length === 0 ? 'Enter your query in the search field' : 'Popular gifs according to your request'}
-                        
+                        {
+                            gifList.data
+                                ? (gifList.data.length === 0 ? 'Enter your query in the search field' : 'Popular gifs according to your request')
+                                : 'Enter your query in the search field'
+                        }
                     </div>
                     <div className="form__popular-wrapper form__wrapper">
-                        {gifList.length === 0 ? <Skeleton/> : renderItems(gifList)}
+                        {
+                            gifList.data
+                                ? (gifList.data.length === 0 ? <Skeleton/> : renderItems(gifList))
+                                :  <Skeleton/>
+                        }
                     </div>
                 </div>
+                {
+                    gifList.data
+                        ? (gifList.data.length > 49 ? null : <div className="form__more" onClick={loadMore}>Load more</div>)
+                        : null
+                }
             </div>
         </div>
     )
